41329e2be325bfc2d281035fe9de01f74e97f3ec
dermo <dermot.logue@dkit.ie>
2023-02-17 13:02:19 +0000

    changes student names to studentIDs

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,63 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 /**
- * Hello world!
- *
+ * Recording students who attend lectures.
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Record of Lectures and the Students who attended each lecture");
+
+        ArrayList<Lecture> lectures = new ArrayList<>();
+
+        populateLectures(lectures);
+
+        for (Lecture l : lectures)
+            System.out.println(l);
+
+
+        //TODO Q5 - see questions to answer and write code here.
+
+
+
+    }   // end main()
+
+    /**
+     * Fill the lectures List with data
+     *
+     * @param lectures
+     */
+    public static void populateLectures(ArrayList<Lecture> lectures) {
+
+        HashSet<String> students = new HashSet<>();
+        students.add("101");
+        students.add("102");
+        students.add("103");
+        students.add("104");
+
+        lectures.add(new Lecture("2023-02-20T14:00:00", "Quantum Entanglement",
+                                    "James Bloom", students));
+
+        students = new HashSet<>();
+        students.add("101");
+        students.add("102");
+        students.add("105");
+        students.add("106");
+
+        lectures.add(new Lecture("2023-02-27T14:00:00", "Lambda functions",
+                                "Josh Bluth", students));
+
+        students = new HashSet<>();
+        students.add("102");
+        students.add("103");
+        students.add("106");
+        students.add("107");
+
+        lectures.add(new Lecture("2023-03-12T14:00:00", "Rust programming",
+                                "Alex Cruz", students));
+
     }
+
 }
--- src/main/java/org/example/Lecture.java
+++ src/main/java/org/example/Lecture.java
@@ -1,2 +1,37 @@
-package org.example;public class Lecture {
+package org.example;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Lecture {
+    private LocalDateTime dateTime;
+    private String topic;
+    private String lecturerName;
+    private Set<String> students;
+
+    public Lecture( String strDateTime, String topic, String lecturerName, Set<String> attendees) {
+        dateTime = LocalDateTime.parse(strDateTime);  // 2pm
+        this.topic = topic;
+        this.lecturerName = lecturerName;
+        this.students = new HashSet<>(attendees);   // make a copy of the set
+    }
+
+    public void addStudentAttendeeName( String studentID ) {
+        students.add(studentID);
+    }
+
+    public Set<String> getStudentAttendeeSet() {
+        return students;        // returns a reference to set of attendees
+    }
+
+    @Override
+    public String toString() {
+        return "Lecture{" +
+                "dateTime=" + dateTime +
+                ", topic='" + topic + '\'' +
+                ", lecturerName='" + lecturerName + '\'' +
+                ", studentAttendees=" + students +
+                '}';
+    }
 }
